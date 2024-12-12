a91606fb0ec475c962c6c536ac5d24f79969ce28
Marat200118 <marat.samigullin1@gmail.com>
2024-12-12 18:06:50 +0100

    added config file

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+
+export default defineConfig({
+  root: "src/",
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+    sourcemap: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "/index.html"),
+        marius: resolve(__dirname, "/marius.html"),
+        dome: resolve(__dirname, "/dome.html"),
+      },
+    },
+  },
+  server: {
+    cors: true,
+  },
+});
